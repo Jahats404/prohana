553cb7fae9680fcae50223fd1e3a967040d6ef87
Babar <rizkiakbar14@gmail.com>
2024-07-31 21:37:18 +0700

    Update forgetPasswordLink.blade.php

--- resources/views/auth/forgetPasswordLink.blade.php
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -12,9 +12,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
@@ -32,8 +30,9 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"><img
-                                    src="{{ asset('img/LOGOPRORORO.png') }}" alt="Logo" class="img-fluid"></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                                <img src="{{ asset('img/LOGOPRORORO.png') }}" alt="Logo" class="img-fluid">
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -48,50 +47,39 @@
                                         @csrf
                                         <input type="hidden" name="token" value="{{ $token }}">
 
-                                        <div class="form-group row">
-                                            <label for="email_address"
-                                                class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                            <div class="col-md-6">
-                                                <input type="text" id="email_address" class="form-control"
-                                                    name="email" required autofocus>
-                                                @if ($errors->has('email'))
-                                                    <span class="text-danger">{{ $errors->first('email') }}</span>
-                                                @endif
-                                            </div>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="email_address" name="email" placeholder="Masukkan Email"
+                                                required autofocus>
+                                            @if ($errors->has('email'))
+                                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                                            @endif
                                         </div>
 
-                                        <div class="form-group row">
-                                            <label for="password"
-                                                class="col-md-4 col-form-label text-md-right">Password</label>
-                                            <div class="col-md-6">
-                                                <input type="password" id="password" class="form-control"
-                                                    name="password" required autofocus>
-                                                @if ($errors->has('password'))
-                                                    <span class="text-danger">{{ $errors->first('password') }}</span>
-                                                @endif
-                                            </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password" name="password" placeholder="Password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                                            @endif
                                         </div>
 
-                                        <div class="form-group row">
-                                            <label for="password-confirm"
-                                                class="col-md-4 col-form-label text-md-right">Confirm Password</label>
-                                            <div class="col-md-6">
-                                                <input type="password" id="password-confirm" class="form-control"
-                                                    name="password_confirmation" required autofocus>
-                                                @if ($errors->has('password_confirmation'))
-                                                    <span
-                                                        class="text-danger">{{ $errors->first('password_confirmation') }}</span>
-                                                @endif
-                                            </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password-confirm" name="password_confirmation" placeholder="Konfirmasi Password" required>
+                                            @if ($errors->has('password_confirmation'))
+                                                <span class="text-danger">{{ $errors->first('password_confirmation') }}</span>
+                                            @endif
                                         </div>
 
-                                        <div class="col-md-6 offset-md-4">
-                                            <button type="submit" class="btn btn-primary">
-                                                Reset Password
-                                            </button>
-                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Reset Password
+                                        </button>
                                     </form>
                                     <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="{{ route('login') }}">Kembali ke Halaman Login</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
